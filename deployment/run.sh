a0e35fbdc0d8010f7a42994c36367eff6412c159
Delai Resgista Setyawan <119137716+delairs@users.noreply.github.com>
2026-03-26 15:44:49 +0700

    deployment

--- deployment/run.sh
+++ deployment/run.sh
@@ -3,5 +3,9 @@ set -e
 
 echo "Loading env from Vault..."
 
-exec envconsul -config=/vault/secrets/envconsul.hcl -- \
-php artisan serve --host=0.0.0.0 --port=8000
+exec envconsul -config=/vault/secrets/envconsul.hcl -- sh -c '
+  echo "envconsul started"
+  echo "APP_ENV=$APP_ENV"
+  echo "APP_DEBUG=$APP_DEBUG"
+  exec php artisan serve --host=0.0.0.0 --port=8000
+'
